1e5443d311f9fb0d3926a568db0d673e965ea7cf
Keiya Chinen <keiya_21@yahoo.co.jp>
2012-06-17 19:08:13 +0900

    first

---
+++ indexer.pl
@@ -0,0 +1,144 @@
+#!/usr/bin/perl --
+
+use strict;
+use warnings;
+
+$| = 1;
+
+BEGIN {
+	use File::Basename;
+	
+	use lib dirname($0).'/lib';
+	
+	my $nativesha = 1;
+	unless (eval "use Digest::SHA ; 1") {
+		warn "Digest::SHA not found. using Digest::SHA::PurePerl.\n";
+		eval "use Digest::SHA::PurePerl";
+		$nativesha = undef;
+	}
+	sub is_nativesha {
+		return $nativesha;
+	}
+}
+
+use Encode;
+use File::Find;
+use MP3::Tag;
+use MP4::Info;
+use Text::Ngram::MySQL::FullText;
+
+my $nativesha = is_nativesha();
+
+my $target_path = $ARGV[0] ? $ARGV[0] : './';
+
+my @found_files = &traverse_files($target_path);
+
+foreach my $file (@found_files) {
+	my %meta;
+	if (my $mp4 = get_mp4tag($file->[2])){
+		$meta{'title'} = $mp4->{TITLE};
+		$meta{'artist'} = $mp4->{ARTIST};
+		$meta{'album'} = $mp4->{ALBUM};
+		$meta{'comment'} = $mp4->{CMT};
+		$meta{'year'} = $mp4->{YEAR};
+		$meta{'genre'} = $mp4->{GENRE};
+		$meta{'time'} = $mp4->{SECS};
+		$meta{'bpm'} = $mp4->{TMPO};
+		$meta{'track'} = $mp4->{TRKN}->[0];
+		$meta{'tracktotal'} = $mp4->{TRKN}->[1];
+		$meta{'disk'} = $mp4->{DISK}->[0];
+		$meta{'disktotal'} = $mp4->{DISK}->[1];
+	}
+	elsif (my $mp3 = MP3::Tag->new($file->[2])) {
+		my $track;
+		($meta{'title'},
+		$track,
+		$meta{'artist'},
+		$meta{'album'},
+		$meta{'comment'},
+		$meta{'year'},
+		$meta{'genre'}) = $mp3->autoinfo();
+		($meta{'track'},$meta{'tracktotal'}) = split('/',$track,2);
+		if (exists $mp3->{ID3v2}) {
+			$meta{'publisher'} = ($mp3->{ID3v2}->get_frame("TPUB"))[0];
+			$meta{'bpm'} = ($mp3->{ID3v2}->get_frame("TBPM"))[0];
+			$meta{'key'} = ($mp3->{ID3v2}->get_frame("TKEY"))[0];
+			$meta{'time'} = ($mp3->{ID3v2}->get_frame("TIME"))[0];
+			$meta{'isrc'} = ($mp3->{ID3v2}->get_frame("TSRC"))[0];
+			if (my $tit2 = ($mp3->{ID3v2}->get_frame("TIT2"))[0]) {
+				$meta{'title'} = $tit2;
+			}
+			$meta{'subtitle'} = ($mp3->{ID3v2}->get_frame("TIT3"))[0];
+		}
+		$mp3->close();
+	}
+	else {
+		next;
+	}
+	
+	warn "[".$file->[2]."]\n";
+	
+	my $sha;
+	if ($nativesha) {
+		$sha = Digest::SHA->new(224);
+	}
+	else {
+		$sha = Digest::SHA::PurePerl->new(224);
+	}
+	$sha->addfile($file->[2]);
+	$meta{'sha2hex'} = $sha->hexdigest();
+	my ($relative_path,$escaped_target_path);
+	if ($^O eq 'MSWin32') {
+		$relative_path = Encode::decode('cp932',$file->[2]);
+		$escaped_target_path = Encode::decode('cp932',$target_path);
+	}
+	else {
+		$relative_path = $file->[2];
+		$escaped_target_path = $target_path;
+	}
+
+	$escaped_target_path =~ s/\\/\\\\/g;
+	$relative_path =~ s/${escaped_target_path}//;
+	$meta{'path'} = $relative_path;
+
+	&build_sql(\%meta);
+}
+
+sub build_sql {
+	no warnings;
+	my $ngram = Text::Ngram::MySQL::FullText->new();
+	$_[0]{'ngram_title'} = $ngram->to_fulltext($_[0]{'title'});
+	$_[0]{'ngram_subtitle'} = $ngram->to_fulltext($_[0]{'subtitle'});
+	$_[0]{'ngram_artist'} = $ngram->to_fulltext($_[0]{'artist'});
+	$_[0]{'ngram_album'} = $ngram->to_fulltext($_[0]{'album'});
+	
+	my $pairs;
+	foreach my $k (keys $_[0]) {
+		if (defined $_[0]->{$k}) {
+			$pairs .= '`'.$k.'`=\''.$_[0]->{$k}.'\',';
+		}
+		else {
+			$pairs .= '`'.$k.'`=NULL,';
+		}
+	}
+	chop $pairs;
+my $table_name = $ARGV[1] ? $ARGV[1] : 'songs';
+
+my $sql =<<__EOS__;
+INSERT INTO $table_name SET
+$pairs
+ON DUPLICATE KEY UPDATE
+$pairs;
+__EOS__
+	
+	print $sql;
+}
+
+sub traverse_files {
+	my @found_files;
+	find(sub {
+		return if $_ !~ /\.mp3$|\.m4a$|\.mp4$/;
+		push @found_files,[$File::Find::dir,$_,$File::Find::name];
+	}, ($_[0]));
+	return @found_files;
+}
---
+++ lib/Text/Ngram/MySQL/FullText.pm
@@ -0,0 +1,204 @@
+package Text::Ngram::MySQL::FullText;
+use warnings;
+use strict;
+use Encode qw/encode decode is_utf8/;
+our $VERSION = '0.02';
+
+sub new {
+	my $class = shift;
+	return bless {
+		window_size => 2,
+		column_name => 'myFullTextColumn',
+		delimiters  => q{ ,.":;()\[\]\{\}!\?\-\|　！？。、・：；『』「」《》〔〕<>()“”‘’≪≫【】〈〉＜＞},
+                return_bind => 0,
+		@_,
+	}, $class;
+}
+
+sub to_fulltext {
+	my $self = shift;
+	return $self->_parse('fulltext',@_);
+}
+
+sub to_query {
+	my $self = shift;
+	return $self->_parse('query',@_);
+}
+
+sub to_match_sql {
+	my $self = shift;
+	my $text = shift;
+        my $bind = shift || $self->{return_bind};
+	return if !defined $text;
+	my $ngram = $self->to_query( $text );
+        if( $bind ){
+            return ( 
+                qq{MATCH($self->{column_name}) }
+                    .qq{AGAINST(? IN BOOLEAN MODE)},
+                $ngram,
+            );
+        }else{
+            return qq{MATCH($self->{column_name}) }
+                .qq{AGAINST('$ngram' IN BOOLEAN MODE)};
+        }
+}
+
+sub _parse {
+	my $self = shift;
+	my $type = shift || 'fulltext';
+	my $text = shift;
+
+	return if !defined $text;
+	$text = decode('utf8', $text) unless is_utf8($text);
+	
+	my $regexp = decode 'utf8', "[$self->{delimiters}]";
+	my @chunks = split /$regexp/, $text;
+
+	my @ngrams;
+	foreach (@chunks){
+		next if $_ eq '';
+                if( $_ =~ /^[a-zA-Z0-9]+$/ ){
+                    if( $type eq 'fulltext' ){
+                        push @ngrams, $_;
+                    }else{
+                        push @ngrams, "+$_*";
+                    }
+		}elsif( $type eq 'fulltext' ){
+			push @ngrams, $self->_make_ngram_fulltext( $_ );
+		}else{
+			push @ngrams, $self->_make_ngram_query( $_ );
+		}
+	}
+	return join(' ', @ngrams);
+}
+
+sub _make_ngram_fulltext {
+	my $self = shift;
+	my $text = shift;
+	return if !defined $text;
+
+	my @ngrams;
+	for my $i (0 .. length($text) - 1){
+		my $str = substr $text, $i, $self->{window_size};
+		#str = encode 'utf8', $str;
+		push @ngrams, $str;
+	}
+	return join(' ', @ngrams);
+}
+
+sub _make_ngram_query {
+	my $self = shift;
+	my $text = shift;
+	return if !defined $text;
+
+	if (length $text < $self->{window_size}){
+		#my $str = encode 'utf8', $text;
+		my $str = $text;
+		$str = $self->_make_mysql_escape_string($str);
+		return "+$str*";
+	}
+
+	my @ngrams;
+	for my $i (0 .. length($text) - $self->{window_size}){
+		my $str = substr $text, $i, $self->{window_size};
+		#$str = encode 'utf8', $str;
+		$str = $self->_make_mysql_escape_string($str);
+		push @ngrams, "+$str";
+	}
+	return join(' ', @ngrams);
+}
+
+sub _make_mysql_escape_string {
+	my $self = shift;
+	my $text = shift;
+	$text =~ s{\\}{\\\\}g;
+	$text =~ s{'}{\\'}g;
+	return $text;
+}
+
+
+=head1 NAME
+
+Text::Ngram::MySQL::FullText - create ngram text for fulltext indexes, with non-ascii character support
+
+=head1 SYNOPSIS
+
+Getting Ready - Inserting Record Into Table
+
+    use Text::Ngram::MySQL::FullText;
+    my $p = Text::Ngram::MySQL::FullText->new();
+    
+    my $text = $p->to_fulltext( 'あいうえお' );
+    # あい いう うえ えお お
+
+    my $dbh = DBI->connect( ... );
+    $dbh->do( qq{ INSERT INTO foo ( id, myText )
+    	VALUES( undef, $text ) } );
+
+Table Search - Fast Search Using FullText Index
+
+    use Text::Ngram::MySQL::FullText;
+    my $p = Text::Ngram::MySQL::FullText->new();
+    my $match_sql = $p->to_match_sql( 'あいうえお' );
+	# MATCH( myText ) AGAINST( '+あい +いう +うえ +えお'
+	# IN BOOLEAN MODE)
+
+    my $dbh = DBI->connect( ... );
+    my $res = $dbh->selectrow_hashref(
+    	qq{ SELECT * FROM foo WHERE $match_sql } );
+    ...
+
+=head1 AUTHOR
+
+Toshimasa Ishibashi, C<< <iandeth at gmail.com> >>
+
+some modified by Keiya Chinen <keiya_21@yahoo.co.jp>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to
+C<bug-text-ngram-mysql-fulltext at rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Text-Ngram-MySQL-FullText>.
+I will be notified, and then you'll automatically be notified of progress on
+your bug as I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Text::Ngram::MySQL::FullText
+
+You can also look for information at:
+
+=over 4
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Text-Ngram-MySQL-FullText>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Text-Ngram-MySQL-FullText>
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Text-Ngram-MySQL-FullText>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Text-Ngram-MySQL-FullText>
+
+=back
+
+=head1 ACKNOWLEDGEMENTS
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2007 Toshimasa Ishibashi, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
+1; # End of Text::Ngram::MySQL::FullText
